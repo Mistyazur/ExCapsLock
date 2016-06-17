5219ac23d05537c2bec3ef2ed07aacdfe912f01e
Aceterni <Aceterni@gmail.com>
2016-06-17 18:58:08 +0800

    keyboard hook

--- keyboardhook.cpp
+++ keyboardhook.cpp
@@ -1,75 +1,91 @@
-/****************************************************************************
-** Meta object code from reading C++ file 'keyboardhook.h'
-**
-** Created by: The Qt Meta Object Compiler version 67 (Qt 5.6.1)
-**
-** WARNING! All changes made in this file will be lost!
-*****************************************************************************/
+#include "keyboardhook.h"
+#include "cmd.h"
+#include <QDebug>
 
-#include "D:/test/keyboardhook.h"
-#include <QtCore/qbytearray.h>
-#include <QtCore/qmetatype.h>
-#if !defined(Q_MOC_OUTPUT_REVISION)
-#error "The header file 'keyboardhook.h' doesn't include <QObject>."
-#elif Q_MOC_OUTPUT_REVISION != 67
-#error "This file was generated using the moc from 5.6.1. It"
-#error "cannot be used with the include files from this version of Qt."
-#error "(The moc has changed too much.)"
-#endif
+Cmd *g_cmd = nullptr;
 
-QT_BEGIN_MOC_NAMESPACE
-struct qt_meta_stringdata_KeyboardHook_t {
-    QByteArrayData data[3];
-    char stringdata0[22];
-};
-#define QT_MOC_LITERAL(idx, ofs, len) \
-    Q_STATIC_BYTE_ARRAY_DATA_HEADER_INITIALIZER_WITH_OFFSET(len, \
-    qptrdiff(offsetof(qt_meta_stringdata_KeyboardHook_t, stringdata0) + ofs \
-        - idx * sizeof(QByteArrayData)) \
-    )
-static const qt_meta_stringdata_KeyboardHook_t qt_meta_stringdata_KeyboardHook = {
-    {
-QT_MOC_LITERAL(0, 0, 12), // "KeyboardHook"
-QT_MOC_LITERAL(1, 13, 7), // "showCmd"
-QT_MOC_LITERAL(2, 21, 0) // ""
-
-    },
-    "KeyboardHook\0showCmd\0"
-};
-#undef QT_MOC_LITERAL
-
-static const uint qt_meta_data_KeyboardHook[] = {
-
- // content:
-       7,       // revision
-       0,       // classname
-       0,    0, // classinfo
-       1,   14, // methods
-       0,    0, // properties
-       0,    0, // enums/sets
-       0,    0, // constructors
-       0,       // flags
-       1,       // signalCount
-
- // signals: name, argc, parameters, tag, flags
-       1,    0,   19,    2, 0x06 /* Public */,
-
- // signals: parameters
-    QMetaType::Void,
-
-       0        // eod
-};
-
-void KeyboardHook::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id, void **_a)
+void KeyClick(DWORD key)
 {
-    if (_c == QMetaObject::InvokeMetaMethod) {
-        KeyboardHook *_t = static_cast<KeyboardHook *>(_o);
-        Q_UNUSED(_t)
-        switch (_id) {
-        case 0: _t->showCmd(); break;
-        default: ;
+    keybd_event(key, 0, 0, 0);
+    keybd_event(key, 0, KEYEVENTF_KEYUP, 0);
+}
+
+LRESULT CALLBACK KbHookProc(int nCode, WPARAM wParam, LPARAM lParam)
+{
+    static bool capsDown = false;
+    static bool keyDown = false;
+    static WPARAM prevWParam = 0;
+    static DWORD prevVKeyCode = 0;
+    PKBDLLHOOKSTRUCT pKey = (PKBDLLHOOKSTRUCT)lParam;
+
+    if (prevWParam != wParam || prevVKeyCode != pKey->vkCode)
+    {
+        qDebug()<<"KEY:"<<wParam<<pKey->vkCode;
+        if (wParam == WM_KEYDOWN)
+        {
+            if (pKey->vkCode == VK_OEM_3)
+            {
+                capsDown = true;
+                keyDown = false;
+            }
+            else
+            {
+                if (capsDown)
+                {
+                    keyDown = true;
+
+                    if (pKey->vkCode == VK_SPACE)
+                    {
+                        // Capslock + space: show command
+                        qDebug()<<"Show";
+                        g_cmd->show();
+                    }
+                }
+                else
+                {
+                    if (pKey->vkCode == VK_ESCAPE)
+                    {
+                        // Esc: hide command
+                        g_cmd->hide();
+                    }
+                }
+            }
         }
-    } else if (_c == QMetaObject::IndexOfMethod) {
-        int *result = reinterpret_cast<int *>(_a[0]);
-        void **func = reinterpret_cast<void **>(_a[1]);
-   
+        else if (wParam == WM_KEYUP)
+        {
+            if (pKey->vkCode == VK_OEM_3)
+            {
+                capsDown = false;
+
+                // If no key pressed after capslock then simulate ESC
+                if (!keyDown)
+                {
+                    KeyClick(VK_ESCAPE);
+                }
+            }
+        }
+
+        prevWParam = wParam;
+        prevVKeyCode = pKey->vkCode;
+    }
+
+    // Discard this message if it's my business
+    if (pKey->vkCode == VK_OEM_3 || capsDown)
+        return true;
+
+    return CallNextHookEx(NULL, nCode, wParam, lParam);
+}
+
+KeyboardHook::KeyboardHook(QObject *parent) : QObject(parent)
+{
+    g_cmd = new Cmd();
+    HHOOK keyboardHook = ::SetWindowsHookEx(WH_KEYBOARD_LL,
+                                            KbHookProc,
+                                            ::GetModuleHandle(NULL),
+                                            0);
+}
+
+KeyboardHook::~KeyboardHook()
+{
+    delete g_cmd;
+}
