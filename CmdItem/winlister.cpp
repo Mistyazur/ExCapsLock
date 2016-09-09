3c020e4a0ae6cf3200059c7494c1b8d29892b43e
Aceterni <Aceterni@gmail.com>
2016-09-09 16:08:04 +0800

    Window list

--- CmdItem/winlister.cpp
+++ CmdItem/winlister.cpp
@@ -26,12 +26,9 @@ bool winLister::exec()
         return false;
 
     pe32.dwSize = sizeof(PROCESSENTRY32);
-    if (::Process32First(hProcessSnap, &pe32))
-    {
+    if (::Process32First(hProcessSnap, &pe32)) {
         do {
-
             procIdNameHash.insert(pe32.th32ProcessID, QString::fromWCharArray(pe32.szExeFile));
-
         } while (::Process32Next(hProcessSnap, &pe32));
     }
 
@@ -39,41 +36,45 @@ bool winLister::exec()
 
     // Get windows
 
-    WINDOWINFO winInfo = {sizeof(WINDOWINFO), };
     HWND hWnd = ::GetTopWindow(NULL);
+    WCHAR szTitle[1024];
+    DWORD dwPid;
+    WINDOWINFO desktopInfo;
+    desktopInfo.cbSize = sizeof(WINDOWINFO);
+    WINDOWINFO winInfo;
+    winInfo.cbSize = sizeof(WINDOWINFO);
 
-    do
-    {
-        ::GetWindowInfo(hWnd, &winInfo);
-        if (!(winInfo.dwStyle & WS_DISABLED)
-                && (winInfo.dwStyle & WS_VISIBLE)
-                && !(winInfo.dwExStyle & WS_EX_TRANSPARENT))
-        {
-            if ((winInfo.rcWindow.left != 0) &&
-                    (winInfo.rcWindow.top != 0) &&
-                    (winInfo.rcWindow.right != 0) &&
-                    (winInfo.rcWindow.bottom != 0))
-            {
-                WCHAR szTitle[1024];
-                ::GetWindowText(hWnd, szTitle, 1024);
+    if ( !::GetWindowInfo(::GetDesktopWindow(), &desktopInfo) )
+        return false;
 
-                DWORD dwPid;
-                ::GetWindowThreadProcessId(hWnd, &dwPid);
+    do {
+        if ( !::GetWindowInfo(hWnd, &winInfo) )
+            continue;
 
-                QString processName = procIdNameHash.value(dwPid, "Unknown");
-                QString &title = QString::fromWCharArray(szTitle);
-                if (title.isEmpty())
-                    title = "None";
+        if ( !(winInfo.dwStyle & WS_VISIBLE) ||
+             (winInfo.dwExStyle & WS_EX_NOACTIVATE) )
+            continue;
 
-                WinControllor *winCtrl = new WinControllor(processName.left(processName.lastIndexOf(".")), this);
-                winCtrl->setTitle(title);
-                winCtrl->setProcess(processName);
-                winCtrl->setId(hWnd);
-                m_resultModel->setItem(index, winCtrl);
+        if ( (winInfo.rcWindow.left == desktopInfo.rcWindow.left) &&
+//             && (winInfo.rcWindow.top == desktopInfo.rcWindow.top)
+//             && (winInfo.rcWindow.bottom == desktopInfo.rcWindow.bottom)
+             (winInfo.rcWindow.right == desktopInfo.rcWindow.right) )
+            continue;
 
-                ++index;
-            }
-        }
+        ::GetWindowThreadProcessId(hWnd, &dwPid);
+        ::GetWindowText(hWnd, szTitle, 1024);
+
+        QString processName = procIdNameHash.value(dwPid, "Unknown");
+        QString &title = QString::fromWCharArray(szTitle);
+        title = title.isEmpty() ? "None" : title;
+
+        WinControllor *winCtrl = new WinControllor(processName.left(processName.lastIndexOf(".")), this);
+        winCtrl->setTitle(title);
+        winCtrl->setProcess(processName);
+        winCtrl->setId(hWnd);
+        m_resultModel->setItem(index, winCtrl);
+
+        ++index;
     } while ((hWnd = ::GetWindow(hWnd, GW_HWNDNEXT)) != NULL);
 
     return true;
